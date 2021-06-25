2dde07b89d7a9c9d405649023d5784180e967a55
oguzadali <ouzadali1@gmail.com>
2021-06-26 01:14:35 +0300

    add loading component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import SearchBar from './components/searchbarComp/Searchbar'
 import ImageList from './components/imgComp/ImageList'
+import Loading from './components/loading/Loading'
 import './App.css'
 
 const urlUnsplash = "https://api.unsplash.com"
@@ -9,24 +10,32 @@ const authkey = process.env.REACT_APP_UNSPLASH_PUBLIC_AUTH_KEY
 
 export default class App extends Component {
 
-  state = { images: [] }
+  state = { images: [], loading: false, error: null }
 
   onSearchImage = async (search) => {
     const result = await axios.get(urlUnsplash + "/search/photos", {
       params: { query: search, per_page: 15 },
       // headers: { Authorization: 'Client-ID anykey' }
       headers: { Authorization: authkey }
-    })
-    // .then((d) => { console.log(d.data.results) })
-    this.setState({ images: result.data.results })
-    // console.log('app:' + search)
+    }).catch(err => this.setState({ error: err, loading: false }))
+
+    this.setState({ loading: true, images: result.data.results, })
+
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 2000);
+
   }
 
   render() {
+    const { loading, error } = this.state
+
+    if (error) { return <p>Oops.. try again</p> }
+
     return (
       <div className="app-container">
         <SearchBar onSearchImage={this.onSearchImage} ></SearchBar>
-        <ImageList images={this.state.images}></ImageList>
+        {loading ? <Loading /> : <ImageList images={this.state.images}></ImageList>}
       </div>
     )
   }
---
+++ src/components/loading/Loading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Loading.css'
+
+export default function Loading() {
+    return (
+        <div className="spinnerr">
+            <div className="lds-spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
